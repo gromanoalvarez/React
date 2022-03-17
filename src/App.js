f414a61651689d22f2f968d6029141f7103ebca8
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-03-16 22:37:14 -0300

    reestructuracion de carpetas y creacion de mi primer componente

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/images/logo.svg";
+import "./assets/css/App.css";
+//Importar componentes:
+import MiPrimerComponente from "./components/MiPrimerComponente";
 
-function HolaMundo(profesion, edad){
+function HolaMundo(profesion, edad) {
   const texto = (
     <div>
       <h3> Tengo {edad} años </h3>
       <h4> y me dedico al {profesion}</h4>
-      <p>Recordar que en mis variables solo se permite una etiqueta html o puedo incluir varias "dentro de una sola etiqueta"</p>
+      <p>
+        Recordar que en mis variables solo se permite una etiqueta html o puedo
+        incluir varias "dentro de una sola etiqueta"
+      </p>
     </div>
-  )
+  );
 
   return texto;
 }
 
 function App() {
-  const nombre ="German Romano Alvarez"
-  const profesion = "Desarrollo web"
-  const presentacion = <h2>Hola, soy {nombre}</h2>
-  const edad = 31
+  const nombre = "German Romano Alvarez";
+  const profesion = "Desarrollo web";
+  const presentacion = <h2>Hola, soy {nombre}</h2>;
+  const edad = 31;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Estamos en <code>src/App.js</code>.
-          El index.js inserta este componente App en el div con id root del index.html! 
+          Estamos en <code>src/App.js</code>. El index.js inserta este
+          componente App en el div con id root del index.html!
         </p>
-        {alert("En este componente App utilizo LENGUAJE JSX, donde puedo con {} meter javaScript")}
+        {alert(
+          "En este componente App utilizo LENGUAJE JSX, donde puedo con {} meter javaScript"
+        )}
         {presentacion}
         {HolaMundo(profesion, edad)}
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <section className="componentes">
+        <MiPrimerComponente/>
+        <MiPrimerComponente/>
+        <MiPrimerComponente/>
+      </section>
+
       </header>
+
     </div>
   );
 }
---
+++ src/components/MiPrimerComponente.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class MiPrimerComponente extends React.Component{
+
+    render(){
+        return (
+            <h1>Hola, soy MiPrimerComponente</h1>
+        );
+    }
+}
+
+export default MiPrimerComponente;
