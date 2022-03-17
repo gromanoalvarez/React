6f9e232af7b7fbb60374b7eb0fddc0eb071f4469
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-03-17 00:16:24 -0300

    Componente Slider

--- src/App.js
+++ src/App.js
@@ -4,34 +4,37 @@ import "./assets/css/App.css";
 import MiPrimerComponente from "./components/MiPrimerComponente";
 import Peliculas from "./components/Peliculas";
 import Header from "./components/Header";
+import Slider from "./components/Slider";
 
-function HolaMundo(profesion, edad) {
-  const texto = (
-    <div>
-      <h3> Tengo {edad} años </h3>
-      <h4> y me dedico al {profesion}</h4>
-      <p>
-        Recordar que en mis variables solo se permite una etiqueta html o puedo
-        incluir varias "dentro de una sola etiqueta"
-      </p>
-    </div>
-  );
 
-  return texto;
-}
+// function HolaMundo(profesion, edad) {
+//   const texto = (
+//     <div>
+//       <h3> Tengo {edad} años </h3>
+//       <h4> y me dedico al {profesion}</h4>
+//       <p>
+//         Recordar que en mis variables solo se permite una etiqueta html o puedo
+//         incluir varias "dentro de una sola etiqueta"
+//       </p>
+//     </div>
+//   );
+
+//   return texto;
+// }
 
 function App() {
-  const nombre = "German Romano Alvarez";
-  const profesion = "Desarrollo web";
-  const presentacion = <h2>Hola, soy {nombre}</h2>;
-  const edad = 31;
+  // const nombre = "German Romano Alvarez";
+  // const profesion = "Desarrollo web";
+  // const presentacion = <h2>Hola, soy {nombre}</h2>;
+  // const edad = 31;
 
   return (
     <div className="App">
       <Header></Header>
+      <Slider></Slider>
 
       {/* Para hacer un comentario en JSX debo usar {} y dentro comentar como js */}
-      
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Slider.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Slider extends Component{
+
+    render(){
+        return (
+            <div id="slider" class="slider-big">
+            <h1>Stairway to heaven</h1>
+            <a href="blog.html" class="btn-white">Ir al Blog</a>
+        </div>
+        );
+    }
+}
+
+export default Slider;
