33563a90d69802d71be224759521a74bbf1f27fa
gromanoalvarez <gromanoalvarez@alumno.unlam.edu.ar>
2022-03-16 23:02:03 -0300

    Mejores practicas en componente, uso de array con metodo map para recorrerlo y sus key para identificar cada elemento del array

--- src/components/MiPrimerComponente.js
+++ src/components/MiPrimerComponente.js
@@ -1,10 +1,38 @@
-import React from "react";
+//* También puedo hacer de este modo pero no es tan buena práctica:
+// import React from "react";
+// class MiPrimerComponente extends React.Component{
 
-class MiPrimerComponente extends React.Component{
+import React, {Component} from "react";
+
+class MiPrimerComponente extends Component{
 
     render(){
+        let receta = {
+            nombre: 'Pizza',
+            ingredientes: ['Tomate', 'Queso', 'Jamon'],
+            calorias: 300
+        }
+
         return (
-            <h1>Hola, soy MiPrimerComponente</h1>
+            <React.Fragment>
+                <h1>Este es MiPrimerComponente</h1>
+                <h4>Como no puedo retornar mas de una etiqueta html, entonces puedo utilizar React.Fragment para que las agrupe sin tener que crear una etiqueta div o section, es practicamente invisible al inspeccionar</h4>
+                <p>{'Mi Receta de: ' + receta.nombre}</p>
+                <p>{'Calorias: ' + receta.calorias}</p>
+                <ol>
+                    {
+                        receta.ingredientes.map((ingrediente, i) => {
+                            console.log(ingrediente);
+                            return (
+                                <li key= {i}>
+                                    {ingrediente}
+                                </li>
+                            )
+                        })
+                    }
+                </ol>
+                <hr/>
+            </React.Fragment>
         );
     }
 }
